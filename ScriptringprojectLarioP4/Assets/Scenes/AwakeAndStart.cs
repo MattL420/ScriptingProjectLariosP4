22841155e57fc14bf55cd50b007347c8f6a39377
MattL420 <116380725+MattL420@users.noreply.github.com>
2022-11-15 11:46:11 -0800

    Fixed some errors and new code
    Add some new code and fixed the errors that came with them.

---
+++ ScriptringprojectLarioP4/Assets/Scenes/AwakeAndStart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    
+    void Awake ()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    
+
+    void Start ()
+    {
+        Debug.Log("Start called.");
+    }
+}
---
+++ ScriptringprojectLarioP4/Assets/Scenes/BasicSyntax.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsAndSyntax : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
