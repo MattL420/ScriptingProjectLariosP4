b991362d0cf1f0e2b71ce44571daf624daf919fe
MattL420 <116380725+MattL420@users.noreply.github.com>
2022-11-30 11:54:29 -0800

    Added Update and Fixed update
    Added Update and Fixed update

--- ScriptringprojectLarioP4/Assets/Scenes/ScopeAndAccessModifiers.cs
+++ ScriptringprojectLarioP4/Assets/Scenes/ScopeAndAccessModifiers.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ScopeAndAccessModifiers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public int alpha = 5;
+
+
+    private int beta = 0;
+    private int gamma = 5;
+
+
+    private AnotherClass myOtherClass;
+    
+
         
+    void Start ()
+    {
+        alpha = 29;
+
+        myOtherClass = new AnotherClass();
+        myOtherClass.FruitMachine(alpha, myOtherClass.apples);
     }
 
-    // Update is called once per frame
+    
+
+    void Example (int pens, int crayons)
+    {
+        int answer;
+        answer = pens * crayons * alpha;
+        Debug.Log(answer);
+    }
+
+
+
     void Update()
     {
-        
+        Debug.Log("Alpha is set to: " + alpha);
     }
 }
